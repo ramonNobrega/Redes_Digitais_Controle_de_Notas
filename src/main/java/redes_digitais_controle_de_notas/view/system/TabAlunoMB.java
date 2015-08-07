69eb8d5d3d72f7348a2836debb76626f38c899cc
veruskasantos <veruska.santos@ccc.ufcg.edu.br>
2015-08-07 20:32:31 -0300

    Desmarcando DAO e BC e ativando cascade = ALL para editar dados do aluno.

--- src/main/java/redes_digitais_controle_de_notas/view/system/TabAlunoMB.java
+++ src/main/java/redes_digitais_controle_de_notas/view/system/TabAlunoMB.java
@@ -11,7 +11,6 @@ import br.gov.frameworkdemoiselle.message.*;
 import br.gov.frameworkdemoiselle.stereotype.*;
 import br.gov.frameworkdemoiselle.template.*;
 import br.gov.frameworkdemoiselle.transaction.*;
-
 import redes_digitais_controle_de_notas.domain.entity.*;
 import redes_digitais_controle_de_notas.domain.enumeration.*;
 import redes_digitais_controle_de_notas.domain.view.*;
@@ -19,7 +18,7 @@ import redes_digitais_controle_de_notas.business.entity.*;
 import redes_digitais_controle_de_notas.business.process.*;
 import redes_digitais_controle_de_notas.constant.*;
 import redes_digitais_controle_de_notas.exception.*;
-
+import redes_digitais_controle_de_notas.persistence.entity.RoleDAO;
 import redes_digitais_controle_de_notas.domain.entity.Aluno;
 import redes_digitais_controle_de_notas.business.entity.AlunoBC;
 
