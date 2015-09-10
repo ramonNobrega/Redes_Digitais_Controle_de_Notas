5c1e46b943349737b1daccaf8557d9e8d3d6a2f7
veruskasantos <veruska.santos@ccc.ufcg.edu.br>
2015-09-10 17:22:56 -0300

    US11: Criação de página de visualização e edição de notas a partir do perfil do professor

---
+++ src/main/java/redes_digitais_controle_de_notas/view/professor/TabManterDesempenhoBimestralDetailMB.java
@@ -0,0 +1,113 @@
+package redes_digitais_controle_de_notas.view.professor;
+
+import java.util.*;
+
+import javax.annotation.*;
+import javax.inject.Inject;
+
+import br.gov.frameworkdemoiselle.annotation.*;
+import br.gov.frameworkdemoiselle.exception.*;
+import br.gov.frameworkdemoiselle.message.*;
+import br.gov.frameworkdemoiselle.stereotype.*;
+import br.gov.frameworkdemoiselle.template.*;
+import br.gov.frameworkdemoiselle.transaction.*;
+
+import redes_digitais_controle_de_notas.domain.entity.*;
+import redes_digitais_controle_de_notas.domain.enumeration.*;
+import redes_digitais_controle_de_notas.domain.view.*;
+import redes_digitais_controle_de_notas.business.entity.*;
+import redes_digitais_controle_de_notas.business.process.*;
+import redes_digitais_controle_de_notas.constant.*;
+import redes_digitais_controle_de_notas.exception.*;
+
+import redes_digitais_controle_de_notas.business.entity.DesempenhoBimestralBC;
+import redes_digitais_controle_de_notas.domain.entity.DesempenhoBimestral;
+
+@ViewController
+@PreviousView("/professor/tabManterDesempenhoBimestral.xhtml")
+@NextView("/professor/tabManterDesempenhoBimestralDetail.xhtml")
+public class TabManterDesempenhoBimestralDetailMB extends AbstractEditPageBean<DesempenhoBimestral, Integer> {
+
+	private static final long serialVersionUID = 1L;
+
+	@Inject
+	private MessageContext messageContext;
+
+	@Inject
+	@Name("messages")
+	private ResourceBundle bundle;
+
+	@Inject
+	private DesempenhoBimestralBC desempenhoBimestralBC;
+	
+	@Override
+	@Transactional
+	public String insert() {
+		this.desempenhoBimestralBC.insert(getBean());
+		messageContext.add(new DefaultMessage("{pages.msg.insertsuccess}"));
+		return getPreviousView();
+	}
+	
+	@Override
+	@Transactional
+	public String update() {
+		/* TriggerCall[edit.update.calculaMediaBimestral] */
+		calculaMediaBimestral();
+		/* TriggerCall[edit.update.calculaMediaBimestral] */
+		/* TriggerCall[edit.update.calculaMediaFinal] */
+		calculaMediaFinal();
+		/* TriggerCall[edit.update.calculaMediaFinal] */
+		this.desempenhoBimestralBC.update(getBean());
+		messageContext.add(new DefaultMessage("{pages.msg.updatesuccess}"));
+		return getPreviousView();
+	}
+	
+	/* Trigger[edit.update.calculaMediaBimestral] */
+	public void calculaMediaBimestral() {
+			DesempenhoBimestral item = getBean();
+			item.setMediaBimestre((item.getNota1() + item.getNota2() + item.getNota3())/3);
+	}
+	
+	/* Trigger[edit.update.calculaMediaBimestral] */
+	
+	/* Trigger[edit.update.calculaMediaFinal] */
+	public void calculaMediaFinal() {
+				
+				DesempenhoBC desempenhoBC = new DesempenhoBC();
+				Double mediaParcial = 0.0;
+				List<DesempenhoBimestral> desempenhoBimestralList = getBean().getAluno().getDesempenhoBimestrais();
+				for (DesempenhoBimestral bimestre : desempenhoBimestralList) {
+					if(bimestre.getMediaBimestre() != null){
+						mediaParcial += bimestre.getMediaBimestre();
+					}
+				}
+				
+				Desempenho desempenho = getBean().getAluno().getDesempenhos().get(0);
+				mediaParcial = mediaParcial / desempenhoBimestralList.size();
+				desempenho.setMediaParcial(mediaParcial);
+				if (mediaParcial >= 7) {
+					desempenho.setSituacao("APROVADO");
+					desempenho.setMediaFinal(mediaParcial);
+				} else if( mediaParcial < 7 && mediaParcial >= 4){
+					desempenho.setSituacao("FINAL");
+				}else {
+					desempenho.setSituacao("REPROVADO");
+					desempenho.setMediaFinal(mediaParcial);
+				}
+				
+				desempenhoBC.update(desempenho);
+			}
+	/* Trigger[edit.update.calculaMediaFinal] */
+	
+	@Transactional
+	public String delete() {
+		this.desempenhoBimestralBC.delete(getBean().getIdBimestre());
+		return getPreviousView();
+	}
+	
+	@Override
+	protected void handleLoad() {
+		setBean(this.desempenhoBimestralBC.load(getId()));
+	}
+
+}
---
+++ src/main/java/redes_digitais_controle_de_notas/view/professor/TabManterDesempenhoBimestralMB.java
@@ -0,0 +1,88 @@
+package redes_digitais_controle_de_notas.view.professor;
+
+import java.util.*;
+
+import javax.annotation.*;
+import javax.inject.Inject;
+
+import br.gov.frameworkdemoiselle.annotation.*;
+import br.gov.frameworkdemoiselle.exception.*;
+import br.gov.frameworkdemoiselle.message.*;
+import br.gov.frameworkdemoiselle.stereotype.*;
+import br.gov.frameworkdemoiselle.template.*;
+import br.gov.frameworkdemoiselle.transaction.*;
+
+import redes_digitais_controle_de_notas.domain.entity.*;
+import redes_digitais_controle_de_notas.domain.enumeration.*;
+import redes_digitais_controle_de_notas.domain.view.*;
+import redes_digitais_controle_de_notas.business.entity.*;
+import redes_digitais_controle_de_notas.business.process.*;
+import redes_digitais_controle_de_notas.constant.*;
+import redes_digitais_controle_de_notas.exception.*;
+
+import redes_digitais_controle_de_notas.business.entity.DesempenhoBimestralBC;
+import redes_digitais_controle_de_notas.domain.entity.DesempenhoBimestral;
+import redes_digitais_controle_de_notas.security.ContextMB;
+@ViewController
+@PreviousView("/professor/tabManterDesempenhoBimestral.xhtml")
+@NextView("/professor/tabManterDesempenhoBimestralDetail.xhtml")
+public class TabManterDesempenhoBimestralMB extends AbstractListPageBean<DesempenhoBimestral, Integer> {
+
+	private static final long serialVersionUID = 1L;
+
+	@Inject
+	private MessageContext messageContext;
+
+	@Inject
+	@Name("messages")
+	private ResourceBundle bundle;
+
+	@Inject
+	private DesempenhoBimestralBC desempenhoBimestralBC;
+	
+		@Inject
+		private TurmaProfessorBC turmaProfessorBC;
+		
+		@Inject
+		private ProfessorBC professorBC;
+		
+		@Inject
+		private TurmaAlunoBC turmaAlunoBC;
+		
+		@Inject
+		private ContextMB context;
+	
+	       private List<DesempenhoBimestral> desempenhoResultList;
+	/* Button[list.handleResultList] */
+		@Override
+		protected List<DesempenhoBimestral> handleResultList() {
+			
+			calculaMediaBimestral();		
+			
+			Professor professor = professorBC.load(new Long(context.getUser().getId()));
+			desempenhoResultList = new ArrayList<DesempenhoBimestral>();
+			for (DesempenhoBimestral db : desempenhoBimestralBC.findAll()){
+					for(TurmaAluno ta : turmaAlunoBC.findAll()){
+						for(TurmaProfessor tp : turmaProfessorBC.findAll()){
+							if(ta.getTurma().equals(tp.getTurma()) && ta.getAluno().equals(db.getAluno()) && tp.getProfessor().equals(professor)){
+						
+							desempenhoResultList.add(db);
+							}
+						}		
+					}
+				}
+				
+			return desempenhoResultList;
+		}
+	/* Button[list.handleResultList] */
+	
+	/* Trigger[list.handleResultList.calculaMediaBimestral] */
+	public void calculaMediaBimestral() {
+		for (DesempenhoBimestral item : desempenhoBimestralBC.findAll()) {
+			item.setMediaBimestre((item.getNota1() + item.getNota2() + item.getNota3())/3);
+		}
+	}
+	
+	/* Trigger[list.handleResultList.calculaMediaBimestral] */
+
+}
